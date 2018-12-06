45b3046f9e0e107116613591a7d3a4352dd439c8
vira0016 <vira0016@algonquinlive.com>
2018-12-06 16:20:13 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,87 @@
+import React, { Component } from 'react';
+import Note from './Note';
+import NoteForm from './Noteform';
+import { DB_CONFIG } from './config';
+import firebase from 'firebase/app';
+import 'firebase/database';
+import './App.css';
+
+class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.addNote = this.addNote.bind(this);
+    this.removeNote = this.removeNote.bind(this);
+
+    this.app = firebase.initializeApp(DB_CONFIG);
+    this.database = this.app.database().ref().child('notes');
+
+    this.state = {
+      notes: [],
+    }
+  }
+
+  componentWillMount(){
+    const previousNotes = this.state.notes;
+
+   
+    this.database.on('child_added', snap => {
+      previousNotes.push({
+        id: snap.key,
+        noteContent: snap.val().noteContent,
+      })
+
+      this.setState({
+        notes: previousNotes
+      })
+    })
+
+    this.database.on('child_removed', snap => {
+      for(var i=0; i < previousNotes.length; i++){
+        if(previousNotes[i].id === snap.key){
+          previousNotes.splice(i, 1);
+        }
+      }
+
+      this.setState({
+        notes: previousNotes
+      })
+    })
+  }
+
+  addNote(note){
+    this.database.push().set({ noteContent: note});
+  }
+
+  removeNote(noteId){
+    console.log("from the parent: " + noteId);
+    this.database.child(noteId).remove();
+  }
+
+  render() {
+    return (
+      <div className="notesWrapper">
+        <div className="notesHeader">
+          <div className="heading">Simple react app using firebase</div>
+        </div>
+        <div className="notesBody">
+          {
+            this.state.notes.map((note) => {
+              return (
+                <Note noteContent={note.noteContent} 
+                noteId={note.id} 
+                key={note.id} 
+                removeNote ={this.removeNote}/>
+              )
+            })
+          }
+        </div>
+        <div className="notesFooter">
+          <NoteForm addNote={this.addNote} />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/config.js
@@ -0,0 +1,8 @@
+export const DB_CONFIG = {
+    apiKey: "AIzaSyAXB2dLdkc0tmaQVbMUKzQcULj1alUGwsg",
+    authDomain: "simplereactfirebase.firebaseapp.com",
+    databaseURL: "https://simplereactfirebase.firebaseio.com",
+    projectId: "simplereactfirebase",
+    storageBucket: "simplereactfirebase.appspot.com",
+    messagingSenderId: "414020511174"
+  };
